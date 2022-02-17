0c20a475f1bc9bcbe05d299ef7a99402ee415b47
Myint <myint@pomelofashion.com>
2022-02-17 10:29:53 +0700

    edit s3 module

--- s3.tf
+++ s3.tf
@@ -67,7 +67,67 @@ resource "aws_s3_bucket_policy" "max-2022-s3" {
   POLICY
 } */
 
+
 module "pomelo_ml_production" {
+  source  = "terraform-aws-modules/s3-bucket/aws"
+  version = "2.14.1"
+
+  bucket                  = "pomelo-ml-production-2022-02-13"
+  acl                     = "private"
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+  attach_policy           = true
+  policy                  = data.aws_iam_policy_document.pomelo_ml_production.json
+
+  # S3 Bucket Ownership Controls
+  control_object_ownership = true
+  object_ownership         = "BucketOwnerPreferred"
+
+  versioning = {
+    enabled    = true
+    mfa_delete = false
+  }
+
+  tags = merge(local.base_bucket_tags, {
+    Name        = "pomelo-ml-production"
+    Environment = "production"
+  })
+
+  providers = {
+    aws = aws.master
+  }
+}
+
+resource "aws_s3_bucket_metric" "pomelo_ml_production_bucket" {
+  provider = aws.master
+  #TODO:
+  #checkov:skip=CKV_AWS_144: Ensure that S3 bucket has cross-region replication enabled
+  #checkov:skip=CKV_AWS_145: Ensure that S3 buckets are encrypted with KMS by default
+  #checkov:skip=CKV_AWS_52: Ensure S3 bucket has MFA delete enabled
+
+  bucket = module.pomelo_ml_production.s3_bucket_id
+  name   = "pomelo-ml-production-bucket"
+}
+
+data "aws_iam_policy_document" "pomelo_ml_production" {
+  statement {
+    sid       = ""
+    effect    = "Allow"
+    resources = ["arn:aws:s3:::pomelo-ml-production/*"]
+    actions   = ["s3:*"]
+
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::${local.master_account_id}:role/data-engineering"]
+    }
+  }
+  provider = aws.master
+
+}
+
+/* module "pomelo_ml_production" {
 
   source = "terraform-aws-modules/s3-bucket/aws"
 
@@ -114,24 +174,8 @@ module "pomelo_ml_production" {
     Environment = "staging"
   })
 
-  policy = <<POLICY
-{
-"Version": "2012-10-17",
- "Statement": [
-   {
-      "Action": "s3:*",
-      "Effect": "Allow",
-      "Principal": {
-        "AWS": "arn:aws:iam::559190605129:root"
-     },
-        "Resource": "arn:aws:s3:::pomelo-ml-production-2022-02-13/*"
-    }
-]
-}
-  POLICY
-
   providers = {
     aws = aws.master
   }
 
-}
+} */
